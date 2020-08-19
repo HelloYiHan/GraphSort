0f2844494029c981559926a3b2261d2872e7273b
YiHan <250147506@qq.com>
2020-08-19 10:17:50 +0800

    GraphSort

--- run_graphsort.py
+++ run_graphsort.py
@@ -8,7 +8,7 @@ import torch_geometric.data
 import subprocess
 import numpy as np
 
-#arguments
+#1.arguments
 parser = argparse.ArgumentParser(description='Arguments of GraphSort')
 parser.add_argument('--input', '-i', type=str, help = 'Expression data of samples', required = True)
 parser.add_argument('--type', '-t', type=str, help = 'Data type, rnaseq OR microarray)',required = True)
@@ -18,7 +18,7 @@ parser.add_argument('--device', '-d', type=str, help = 'Computation device, gpu 
 parser.add_argument('--size', '-s', type=str, help = 'Size of preprocessing data, 1k OR 2k', default = '1k')
 args = parser.parse_args()
 
-#select device
+#2.select device
 if args.device == 'cpu':
     device = torch.device('cpu')
 elif args.device == 'gpu' and torch.cuda.is_available():
@@ -30,7 +30,7 @@ else:
 
 path = os.path.dirname(os.path.abspath(__file__))
 
-#preprocess
+#3.preprocess
 print('Start preprocessing')
 if args.type == 'rnaseq':
     if args.size == '1k':
@@ -46,28 +46,27 @@ else:
 
 subprocess.call(['chmod', 'u+x', path + '/preprocessing.R'])
 subprocess.call([path + '/preprocessing.R', args.input, train_file, args.type])
+print('Preprocessing done')
 
-#load model
+#4.load model
 model = graphsort_core.Net(num_features = 1, y_size = 8).to(device)
 if args.type == 'rnaseq':
     if device.type == "cuda":
-        model.load_state_dict(torch.load(path + '/trained_models/state1627195299007epoch60.pkl',map_location='cuda'))
+        model.load_state_dict(torch.load(path + '/trained_models/graphsort_rnaseq_model.pkl',map_location='cuda'))
     else:
-        model.load_state_dict(torch.load(path + '/trained_models/state1627195299007epoch60.pkl',map_location='cpu'))
+        model.load_state_dict(torch.load(path + '/trained_models/graphsort_rnaseq_model.pkl',map_location='cpu'))
 elif args.type =='microarray':
     if device.type == "cuda":
-        model.load_state_dict(torch.load(path + '/trained_models/state20386epoch775.pkl',map_location='cuda'))
+        model.load_state_dict(torch.load(path + '/trained_models/graphsort_microarray_model.pkl',map_location='cuda'))
     else:
-        model.load_state_dict(torch.load(path + '/trained_models/state20386epoch775.pkl',map_location='cpu'))
+        model.load_state_dict(torch.load(path + '/trained_models/graphsort_microarray_model.pkl',map_location='cpu'))
 
-#estimation
+#5.estimation
 print('Start estimation')
-
 dataset_input = read_dataset.two_dim_data(zippath= 'InputFile_' + args.input + '.zip', root = osp.join('.', 'data', 'InputFile_' + args.input), name = 'InputFile_' + args.input, use_node_attr = True)
 loader_input = torch_geometric.data.DataLoader(dataset_input, batch_size=args.batch_size)
 if args.type == 'rnaseq':
     graphsort_core.estimateRNASEQ(model, loader_input, device, args.output, args.input)
 elif args.type == 'microarray':
     graphsort_core.estimateMicroarray(model, loader_input, device, args.output, args.input)
-
 print("GraphSort estimation done")
